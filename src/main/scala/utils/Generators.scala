b3d10b62a481774ddd8a3ca416e1d9e677c83e18
birdofartemis <72250724+birdofartemis@users.noreply.github.com>
2022-07-29 00:50:17 +0000

    Improve generator

--- src/main/scala/utils/Generators.scala
+++ src/main/scala/utils/Generators.scala
@@ -2,70 +2,34 @@ package utils
 
 import models.{Item, Order}
 import resources.Data
+import utils.DateHelpers.getRandomDateToOrder
 
-import java.time.{LocalDate, LocalDateTime, LocalTime, Month}
 import scala.annotation.tailrec
 import scala.util.Random.between
 
 object Generators extends Data {
-  def getRandomDate: LocalDateTime =
-    LocalDateTime
-      .of(
-        LocalDate.of(between(2015, 2022), Month.of(between(1, 13)), between(1, 25)),
-        LocalTime.of(between(0, 24), between(0, 60), between(0, 60))
-      )
+  def getRandomElement[A](list: List[A]): A = list(between(0, list.size))
 
-  def getRandomDateToOrder(itemsList: List[Item]): LocalDateTime = {
+  def getRandomStreamOfItems: LazyList[Item] = {
+    val streamSize = between(1, 75)
     @tailrec
-    def mostRecentProduct(currentItem: Item, next: List[Item]): Item = {
-      if (next.isEmpty) currentItem
-      else {
-        if (currentItem.product.creationDate isAfter next.head.product.creationDate)
-          mostRecentProduct(currentItem, next.tail)
-        else mostRecentProduct(next.head, next.tail)
-      }
+    def constructList(size: Int, stream: LazyList[Item]): LazyList[Item] = {
+      if (size == 0) stream
+      else constructList(size - 1, stream.appended(itemsList(between(1, itemsList.size))))
     }
-    val mostRecentDate = mostRecentProduct(itemsList.head, itemsList.tail).product.creationDate
-
-    LocalDateTime
-      .of(
-        LocalDate.of(
-          between(mostRecentDate.getYear, 2022),
-          Month.of(between(mostRecentDate.getMonthValue, 13)),
-          between(mostRecentDate.getDayOfMonth, 25)
-        ),
-        LocalTime.of(
-          between(mostRecentDate.getHour, 24),
-          between(mostRecentDate.getMinute, 60),
-          between(mostRecentDate.getSecond, 60)
-        )
-      )
-
+    constructList(streamSize, LazyList.empty[Item])
   }
-
-  def getRandomItemList: List[Item] = {
-    val listSize = between(1, 75)
-    @tailrec
-    def constructList(size: Int, list: List[Item]): List[Item] = {
-      if (size == 0) list
-      else constructList(size - 1, list.appended(itemsList(between(1, itemsList.size))))
-    }
-    constructList(listSize, List.empty[Item])
-  }
-
-  def getRandomCustomer: String = namesList(between(0, namesList.size))
-  def getRandomAddress: String  = addressList(between(0, addressList.size))
 
   def getRandomOrders: LazyList[Order] = {
-    val streamSize = 1000
+    val streamSize = 100
 
     @tailrec
     def constructList(size: Int, stream: LazyList[Order]): LazyList[Order] = {
       if (size == 0) stream
       else {
-        val customer   = getRandomCustomer
-        val address    = getRandomAddress
-        val items      = getRandomItemList
+        val customer   = getRandomElement(namesList)
+        val address    = getRandomElement(addressList)
+        val items      = getRandomStreamOfItems
         val totalPrice = Order.totalPrice(items)
         val orderDate  = getRandomDateToOrder(items)
         val contact    = Order.customerContact(customer)
